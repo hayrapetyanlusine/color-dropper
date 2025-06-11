90c5a42ca632e72cbf3d3fc9e46ea88e8e107acb
hayrapetyanlusine <lusinehayrapetyan0220@gmail.com>
2025-06-11 12:38:42 +0400

    refactor: canvas component

--- src/app/shared/components/canvas/canvas.component.ts
+++ src/app/shared/components/canvas/canvas.component.ts
@@ -2,68 +2,67 @@ import {
   AfterViewInit,
   Component,
   ElementRef,
-  inject, input,
+  inject,
+  input,
   OnChanges,
+  signal,
   SimpleChanges,
-  ViewChild
+  viewChild,
 } from '@angular/core';
 import { drawImage } from "../../functions/drawImage";
-import { NgClass, NgStyle } from "@angular/common";
 import { getColorAtPoint } from "../../functions/getColorAtPoint";
 import { ColorData } from "../../../interface/colorData";
 import { DataService } from "../../../services/data.service";
 
 @Component({
     selector: 'app-canvas',
-    imports: [NgStyle, NgClass],
+    imports: [],
     templateUrl: './canvas.component.html',
     styleUrl: './canvas.component.css'
 })
 export class CanvasComponent implements AfterViewInit, OnChanges {
-  dataService = inject(DataService);
+  public readonly dataService = inject(DataService);
+  private readonly canvas = viewChild.required<ElementRef>('imgCanvas');
+  public imageData = input();
 
-  @ViewChild('imgCanvas') canvas!: ElementRef<HTMLCanvasElement>;
-  imageData = input();
-
-  cursorStyle: { [key: string]: any } = {};
-  initialRender = true;
-  isMouseOver = false;
+  public cursorStyle = signal<{ [key: string]: any }>({});
+  private initialRender = signal(true);
+  public isMouseOver = signal(false);
 
   ngAfterViewInit(): void {
-    drawImage(this.dataService.fileData(), this.canvas);
-
-    this.initialRender = false;
+    drawImage(this.dataService.fileData(), this.canvas());
+    this.initialRender.set(false);
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes['imageData'] && !this.initialRender) {
-      drawImage(this.imageData(), this.canvas);
+    if (changes['imageData'] && !this.initialRender()) {
+      drawImage(this.imageData(), this.canvas());
     }
   }
 
   onMouseMove(evt: MouseEvent): void {
-    this.isMouseOver = true;
+    this.isMouseOver.set(true);
 
     if (this.dataService.activeCursor()) {
-      const {red, green, blue}: ColorData = getColorAtPoint(evt, this.canvas);
+      const {red, green, blue}: ColorData = getColorAtPoint(evt, this.canvas());
 
       const translateX: number = evt.clientX - 40 / 2;
       const translateY: number = evt.clientY - 40 / 2;
 
-      this.cursorStyle = {
+      this.cursorStyle.set({
         boxShadow: `0 0 0 8px rgb(${red}, ${green}, ${blue})`,
         transform: `translate(${translateX}px, ${translateY}px)`
-      };
+      });
     }
   }
 
   onMouseLeave(): void {
-    this.isMouseOver = false;
+    this.isMouseOver.set(false);
   }
 
   onClick(evt: MouseEvent): void {
     if (this.dataService.activeCursor()) {
-      const {red, green, blue}: ColorData = getColorAtPoint(evt, this.canvas);
+      const {red, green, blue}: ColorData = getColorAtPoint(evt, this.canvas());
 
       this.dataService.updateColor(`rgb(${red}, ${green}, ${blue})`);
     }
